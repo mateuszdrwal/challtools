949aef2ac6064fab11ca6d3136e13eb8ef0d3830
loovjo <jonathan.loov@gmail.com>
2022-05-24 21:58:55 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="challtools",
-    version="0.4.4",
+    version="0.4.6",
     author="Mateusz Drwal",
     author_email="me@mateuszdrwal.com",
     description="A tool for managing CTF challenges and challenge repositories using the OpenChallSpec",
