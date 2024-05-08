0d8397110459d73357712fe1d54803a5cf5e0edd
mateuszdrwal <drwal.mateusz@gmail.com>
2024-05-08 22:31:27 +0200

    migrated to pyproject.toml

--- setup.py
+++ setup.py
@@ -2,15 +2,6 @@ import setuptools
 from pathlib import Path
 
 setuptools.setup(
-    name="challtools",
-    version="0.4.9",
-    author="Mateusz Drwal",
-    author_email="me@mateuszdrwal.com",
-    description="A tool for managing CTF challenges and challenge repositories using the OpenChallSpec",
-    long_description=Path("README.md").read_text(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/mateuszdrwal/challtools",
-    packages=setuptools.find_packages(),
     package_data={
         "challtools": ["challenge.schema.json", "codes.yml"]
         + [
@@ -19,19 +10,4 @@ setuptools.setup(
             if path.is_file()
         ]
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-    entry_points={"console_scripts": ["challtools = challtools.cli:main"]},
-    install_requires=[
-        "PyYAML",
-        "jsonschema>=3.0.0",
-        "docker>=2.3.0,!=3.1.2,!=5.0.0",
-        "requests>=2.0.0",
-        'pypiwin32;platform_system=="Windows"',
-        "argcomplete>=2.0.0",
-    ],
 )
